310b02acac59d02f46b87241cf459b298d0c2cfc
Dongkeun <hongdongk@naver.com>
2023-09-25 23:25:58 +0900

    Create 최대부분증가수열.js

---
+++ DP/최대부분증가수열.js
@@ -0,0 +1,18 @@
+function solution(arr){  
+    let dp = new Array(arr.length).fill(0);
+    dp[0] = 1;
+    for(let i = 1 ; i<arr.length; i++){
+       let max = 0;
+       for(let j = i-1; j>0; j--){
+        if(arr[j]<arr[i] && dp[j]>max){
+            max = dp[j];
+        }
+       }
+       dp[i] = max + 1;
+    }
+    
+    return Math.max(...dp);
+}
+
+let arr=[5, 3, 7, 8, 6, 2, 9, 4];
+console.log(solution(arr));
