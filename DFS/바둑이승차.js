6deef78cea9300d78ddc191ee25cd9a29a9d400f
Dongkeun <hongdongk@naver.com>
2023-10-18 22:04:54 +0900

    Create 바둑이승차.js

---
+++ DFS/바둑이승차.js
@@ -0,0 +1,26 @@
+ // 트리를 그린다!
+ // 풀었음!!!!!!!!!!!!!!!!!!!
+ 
+ function solution(max, array){
+    let answer = 0;
+
+    function DFS(L, sum){
+        if(sum > max){
+            return;
+        }
+        if(L === array.length){
+            answer = Math.max(answer, sum);
+            return;
+        }else{
+            DFS(L+1, sum+array[L]);
+            DFS(L+1, sum);
+        }
+    }
+
+    DFS(0,0);
+
+    return answer;
+
+ }
+
+ console.log(solution(259, [81,58,42,33,61]))
