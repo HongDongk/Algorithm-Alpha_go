3a388075e5de7a2503bbfa71ca8fb80d82637e56
Dongkeun <hongdongk@naver.com>
2023-10-24 22:35:12 +0900

    Create 순열구하기.js

---
+++ DFS/순열구하기.js
@@ -0,0 +1,29 @@
+// 순열구하기 check배열 사용(외운다.)
+
+function solution(m, arr){         
+    let answer=[];
+    let ch = new Array(arr.length).fill(0);
+    let temp = new Array(m).fill(0);
+
+    function DFS(L){
+        if(L === m) {
+            answer.push(temp.slice()); 
+        }
+        else{
+            for(let i = 0; i<arr.length; i++){
+                if(ch[i]===0){
+                    ch[i]=1;
+                    temp[L]=arr[i];
+                    DFS(L+1);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    DFS(0);
+    return answer;
+}
+
+let arr=[3, 6, 9];
+console.log(solution(2, arr));
