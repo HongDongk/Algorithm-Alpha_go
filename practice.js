0c5c638e2761399b426d5d36a4f09b0e55f38eab
Dongkeun <hongdongk@naver.com>
2023-11-22 14:00:20 +0900

    순열구하기

--- practice.js
+++ practice.js
@@ -1,1 +1,25 @@
+<<<<<<< Updated upstream
 
+=======
+let arr = [3, 6, 9];
+let answer = "";
+let temp = new Array(2).fill(0);
+let check = new Array(3).fill(false);
+
+function DFS(L) {
+  if (L === 2) {
+    console.log(temp);
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (check[i] === false) {
+        check[i] = true;
+        temp[L] = arr[i];
+        DFS(L + 1);
+        check[i] = false;
+      }
+    }
+  }
+}
+
+DFS(0);
+>>>>>>> Stashed changes
