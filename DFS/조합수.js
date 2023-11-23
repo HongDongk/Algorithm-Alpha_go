d73467e364d8fbae031abab9fe381068262ec750
Dongkeun <hongdongk@naver.com>
2023-11-23 22:37:01 +0900

    조합수다시풀기

--- DFS/조합수.js
+++ DFS/조합수.js
@@ -1,9 +1,8 @@
 // 재귀함수가 너무 많이 돌게되면 오래걸리므로 
 // 메모이제이션을 통해 구했던 값을 저장하여 재사용 한다!!(DP)
-
 function solution(n, r){     
 
-    let dy = new Array(35).fill(0).map((a) => new Array(35).fill(0)); // 2차원배열 만드는 방법
+    let dy = new Array(n+1).fill(0).map(() => new Array(r+1).fill(0)); // 2차원 배열 dy에 구해놓은 값저장 (ex) dy[3][1] = 3
 
     function DFS(n, r){
         if(dy[n][r]>0) return dy[n][r];
@@ -14,5 +13,18 @@ function solution(n, r){
     return DFS(n, r);
 
 }
+
+// 메모이제이션 없을때 풀이(느림)
+function solution(n, r){     
+
+    function DFS(n, r){
+        if(n===r || r===0) return 1;   // 3C0 = 1, 3C3 = 1;
+        else return DFS(n-1, r-1)+DFS(n-1, r);
+    }
+
+    return DFS(n, r);
+
+}
+
 
 console.log(solution(33, 19));
