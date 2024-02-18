c42fec2264f1a2ba397c9e1133a68e30f4b5c80e
Dongkeun <hongdongk@naver.com>
2024-02-18 21:24:29 +0900

    DFS다시풀기

--- DFS/No.5 조합의 경우수.js
+++ DFS/No.5 조합의 경우수.js
@@ -1,14 +1,18 @@
+// 메모이제이션 사용해서 풀기
+// 확실히 시간단축!
+
 function solution(n, r) {
-  let answer = [];
-  let dy = new Array(35).fill(0).map((a) => new Array(35).fill(0));
+  let dy = new Array(35).fill(0).map(() => new Array(35).fill(0));
 
   function DFS(n, r) {
     if (dy[n][r] > 0) return dy[n][r];
     if (n === r || r === 0) return 1;
     else return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
   }
-  answer = DFS(n, r);
-  return answer;
+
+  DFS(n, r);
+
+  return dy[n][r];
 }
 
-console.log(solution(5, 3));
+console.log(solution(33, 19));
--- practice.js
+++ practice.js
@@ -1,12 +1,43 @@
-var x = 100; // 전역변수
+function solution(n, f) {
+  let answer = [];
+  let ch = new Array(n + 1).fill(false);
+  let temp = new Array(n).fill(0);
+  let dy = new Array(20).fill(0).map(() => new Array(20).fill(0));
+  let combination = [];
 
-{
-  var x = 1; // 전역변수
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else {
+      return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    combination[i] = combi(n - 1, i);
+  }
+
+  function DFS(L) {
+    if (L === n) {
+      for (let i = 0; i < n; i++) {
+        temp.slice()[i] = temp.slice()[i] * combination[i];
+      }
+      answer.push(temp.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === false) {
+          ch[i] = true;
+          temp[L] = i;
+          DFS(L + 1);
+          ch[i] = false;
+        }
+      }
+    }
+  }
+
+  DFS(0);
+
+  return answer;
 }
 
-function ex() {
-  var x = 5; // 지역변수
-  console.log(x); // 5
-}
-
-console.log(x);
+console.log(solution(4, 16));
