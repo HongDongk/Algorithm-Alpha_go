fa22492f2201bdcf5b0ccdf865f819d749b04516
Dongkeun <hongdongk@naver.com>
2024-02-19 22:33:32 +0900

    DFS 마무리

---
+++ DFS/No.7 조합구하기.js
@@ -0,0 +1,22 @@
+// 조합은 재귀를 돌릴때 반복문에서 고른숫자 뒤에서만 선택할수있게 해야함
+
+function solution(n, m) {
+  let answer = [];
+  let temp = new Array(m).fill(0);
+
+  function DFS(L, s) {
+    if (L === m) {
+      answer.push(temp.slice());
+    } else {
+      for (let i = s; i <= n; i++) {
+        temp[L] = i;
+        DFS(L + 1, i + 1);
+      }
+    }
+  }
+
+  DFS(0, 1);
+  return answer;
+}
+
+console.log(solution(4, 2));
--- practice.js
+++ practice.js
@@ -1,43 +1,22 @@
-function solution(n, f) {
+function solution(n, k, arr, m) {
+  let temp = new Array(k).fill(0);
   let answer = [];
-  let ch = new Array(n + 1).fill(false);
-  let temp = new Array(n).fill(0);
-  let dy = new Array(20).fill(0).map(() => new Array(20).fill(0));
-  let combination = [];
-
-  function combi(n, r) {
-    if (dy[n][r] > 0) return dy[n][r];
-    if (n === r || r === 0) return 1;
-    else {
-      return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
-    }
-  }
-
-  for (let i = 0; i < n; i++) {
-    combination[i] = combi(n - 1, i);
-  }
-
-  function DFS(L) {
-    if (L === n) {
-      for (let i = 0; i < n; i++) {
-        temp.slice()[i] = temp.slice()[i] * combination[i];
+  function DFS(L, s) {
+    if (L === k) {
+      if (temp.reduce((a, b) => a + b, 0) % m === 0) {
+        answer.push(temp.slice());
       }
-      answer.push(temp.slice());
     } else {
-      for (let i = 1; i <= n; i++) {
-        if (ch[i] === false) {
-          ch[i] = true;
-          temp[L] = i;
-          DFS(L + 1);
-          ch[i] = false;
-        }
+      for (let i = s; i < n; i++) {
+        temp[L] = arr[i];
+        DFS(L + 1, i + 1);
       }
     }
   }
 
-  DFS(0);
-
+  DFS(0, 0);
   return answer;
 }
 
-console.log(solution(4, 16));
+let arr = [2, 4, 5, 8, 12];
+console.log(solution(5, 3, arr, 6));
