93e22425427a884fab946e954b9797c8e2057f4c
Dongkeun <hongdongk@naver.com>
2024-02-26 12:45:14 +0900

    DFS 마무리

--- DFS/No.12 섬나라아일랜드.js
+++ DFS/No.12 섬나라아일랜드.js
@@ -1,21 +1,22 @@
 function solution(board) {
   let answer = 0;
-  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1]; // 시계방향
   let dy = [0, 1, 1, 1, 0, -1, -1, -1];
 
   function DFS(x, y) {
     board[x][y] = 0;
-    for (let i = 0; i < 8; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-      if (nx >= 0 && ny >= 0 && nx < arr[0].length && ny < arr.length && board[nx][ny] === 1) {
+    for (let k = 0; k < 8; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
         DFS(nx, ny);
       }
     }
   }
 
-  for (let i = 0; i < arr[0].length; i++) {
-    for (let j = 0; j < arr.length; j++) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
       if (board[i][j] === 1) {
         answer++;
         DFS(i, j);
