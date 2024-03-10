590b0a34b2d2671e1e78b8aaee6576be93ca8f4a
Dongkeun <hongdongk@naver.com>
2024-03-10 22:41:27 +0900

    BFS 다시풀기

--- practice.js
+++ practice.js
@@ -1,19 +1,39 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-    this.age = 'age';
+function solution(board) {
+  let answer = 0;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  function DFS(x, y) {
+    board[x][y] = 0;
+    for (let i = 0; i < 8; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
+      if (nx >= 0 && ny >= 0 && nx < board[0].length && ny < board.length && board[nx][ny] === 1) {
+        DFS(nx, ny);
+      }
+    }
   }
 
-  static sayHi() {
-    console.log('안녕! 코린아');
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === 1) {
+        answer++;
+        DFS(i, j);
+      }
+    }
   }
 
-  static sayError() {
-    console.log(this.name, this.age);
-  }
+  return answer;
 }
 
-let person = new Person('Dongkeun');
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
 
-// Person.sayHi(); // 안녕! 코린아
-Person.sayError(); // 안녕! 코린아
+console.log(solution(arr));
