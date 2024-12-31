5b4b4b20e6c7e5ae5c9b3afcc7d59eca5f239aa6
Dongkeun <hongdongk@naver.com>
2024-12-31 11:09:15 +0900

    조합수정

--- DFS/No.4 조합구하기.js
+++ DFS/No.4 조합구하기.js
@@ -1,6 +1,6 @@
 // 조합은 재귀를 돌릴때 반복문에서 고른숫자 뒤에서만 선택할수있게 해야함
 
-function solution(n, m) {
+function solution(arr, m) {
   let answer = [];
   let temp = new Array(m).fill(0);
 
@@ -8,15 +8,15 @@ function solution(n, m) {
     if (L === m) {
       answer.push(temp.slice());
     } else {
-      for (let i = s; i <= n; i++) {
-        temp[L] = i;
+      for (let i = s; i < arr.length; i++) {
+        temp[L] = arr[i];
         DFS(L + 1, i + 1);
       }
     }
   }
 
-  DFS(0, 1);
+  DFS(0, 0);
   return answer;
 }
 
-console.log(solution(4, 2));
+console.log(solution([1, 2, 3, 4], 2));
